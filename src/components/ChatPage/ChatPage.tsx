faeb3372692158ef49bfd17908c2439d8f8a1de6
kharlos04 <kharlos04@gmail.com>
2023-11-28 17:31:00 -0500

    notification view and create elements to join chat

--- src/components/ChatPage/ChatPage.tsx
+++ src/components/ChatPage/ChatPage.tsx
@@ -1,11 +1,25 @@
 import { FiSend } from "react-icons/fi";
 import styles from './ChatPage.module.css'
-
+import { NotificationMessage } from '../NotificationMessage'
 
 export function ChatPage() {
+  const messageLinkCopied = 'Link guardado con el portapapeles';
+  const messageUsedAddedChat = 'Usuario Ricardo Bermudez Ingresado';
 
   return (
     <div className={styles.root}>
+      <div className={styles.copyLinkSection}>
+        <button className={styles.copyLinkButton}><FiSend className={styles.iconCopyLink} /></button>
+        <a className={styles.copyLinkText} href="#">Copiar Link para el chat</a>
+      </div>
+      <div className={styles.notificationContainer}>
+        <NotificationMessage message={messageLinkCopied} className={styles.notificationBox} />
+        <NotificationMessage message={messageUsedAddedChat} className={styles.notificationBox} />
+      </div>
+
+      <div className={styles.messagesChatArea}>
+
+      </div>
 
 
       <div className={styles.sendMessageElements}>
